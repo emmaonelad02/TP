594a37d7e4535e509a5102979894240abb597cb3
emmaonelad02 <153606823+emmaonelad02@users.noreply.github.com>
2024-04-23 12:58:08 +0000

    Add files via upload

---
+++ http-basics.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+	
+	const url = req.url;
+	
+	// page d accueil
+	if (url === '/') {
+		res.writeHead(200, {'content-type': 'text/html'});
+		res.write("<h1>Bienvenue à la page d'accueil</h1>");
+		res.end();
+	} else if(url === '/about') {
+		res.writeHead(200, {'content-type': 'text/html'});
+		res.write("<h1>a propos de nous</h1>");
+		res.end();
+	} else if (url === '/contact') {
+		res.writeHead(200, {'content-type': 'text/html'});
+		res.write("<h1>tel: +221 74 500 10 10</h1>");
+		res.end();
+	} else {
+		res.writeHead(404, {'content-type': 'text/html'});
+		res.write("<h1>oops! page introuvable</h1>");
+		res.end();
+		
+	}
+  
+});
+
+server.listen(8888, "localhost", () => {
+  console.log("listening on port 8888");
+});
